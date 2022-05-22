b5710e56dc21c605897a27310df20e240129ca3e
8210227 / Gilberto <8210227@estg.ipp.pt>
2022-05-22 19:02:10 +0100

    sellBook New Changes

--- frontend/src/app/sell-book/sell-book.component.ts
+++ frontend/src/app/sell-book/sell-book.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sell-book.component.css']
 })
 export class SellBookComponent implements OnInit {
-
-  constructor() { }
-
+  genres = ['Fiction', 'Non-Fiction', 'Children', 'Others', 'Biography', 'Poetry', 'Fantasy', 'Thriller', 'Horror', 'Mystery', 'Romance', 'Self-Help', 'Health', 'Travel', 'Science', 'History', 'Religion', 'Philosophy', 'Psychology', 'Business', 'Comics', 'Art', 'Cooking', 'Drama', 'Education', 'Engineering', 'Finance', 'Health', 'Law', 'Medicine', 'Music', 'Science', 'Sports', 'Technology', 'Travel', 'Youth'];
   ngOnInit(): void {
   }
 
