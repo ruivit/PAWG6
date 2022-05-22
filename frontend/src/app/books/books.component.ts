65ea2d6aef7e9813ea64afb7459903ad88663dfc
8190479 <8190479@estg.ipp.pt>
2022-05-22 11:10:22 +0100

    Actual Fix

--- frontend/src/app/books/books.component.ts
+++ frontend/src/app/books/books.component.ts
@@ -32,14 +32,15 @@ export class BooksComponent implements OnInit {
   }
 
   addToCart(book: Book) {
+    // if the quantity is already at the maximum, alert the user
+    if (this.cartService.items.find(x => x.stock === book.stock)) {
+      window.alert('No more ' + book.title + ' books available!');
+      return;
+    }
+
     // if the book is already in the cart, increment the quantity
     if (this.cartService.items.find(x => x._id === book._id)) {
-      // if the quantity is already at the maximum, alert the user
-      if (this.cartService.items.find(x => x._id === book._id).stock === book.stock) {
-        window.alert('Your product has been added to the cart! + ' + book.title);
-      } else { 
-        this.cartService.items.find(x => x._id === book.stock++);
-      }
+      this.cartService.items.find(x => x._id === book.stock++);
     } else {     
       this.cartService.addToCart(book);
     }
